8da7c368f4d4780770919c7831ea00d7d20724f1
Jinjun Liu <ljj7129@gmail.com>
2023-03-31 16:02:22 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -141,7 +141,7 @@ const Home: NextPage = () => {
                   Your generated advertisement campaign
                 </h2>
               </div>
-              <div className="space-y-8 flex flex-col max-w-xl mx-auto">
+              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
                 <div
                   className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
                   onClick={() => {
