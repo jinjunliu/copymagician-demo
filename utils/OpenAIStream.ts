d60b1fe9d0c8695ec49dc6992bb6c7b2cc585f5f
Jinjun Liu <ljj7129@gmail.com>
2025-03-16 15:50:39 -0500

    update api model and url

--- utils/OpenAIStream.ts
+++ utils/OpenAIStream.ts
@@ -29,7 +29,7 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
 
   let counter = 0;
 
-  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+  const res = await fetch("https://api.jinjunliu.com/v1/chat/completions", {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
